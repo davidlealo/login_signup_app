2c279c00434876befe861843b256743f11e8d369
davidlealo <davidlealo@gmail.com>
2024-11-04 15:55:53 -0300

    Added authentication functionality

---
+++ lib/auth_screen.dart
@@ -0,0 +1,70 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  bool isLogin = true;
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _toggleAuthMode() {
+    setState(() {
+      isLogin = !isLogin;
+    });
+  }
+
+  Future<void> _submit() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      if (isLogin) {
+        await _auth.signInWithEmailAndPassword(email: email, password: password);
+      } else {
+        await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isLogin ? 'Login' : 'Signup'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submit,
+              child: Text(isLogin ? 'Login' : 'Signup'),
+            ),
+            TextButton(
+              onPressed: _toggleAuthMode,
+              child: Text(isLogin ? 'Create new account' : 'I already have an account'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
